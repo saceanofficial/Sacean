dc8ee8d185cf5ce355bb0cc494feae527fc62d0c
saceanofficial <saceanofficial@gmail.com>
2025-10-08 12:11:56 +0800

    Create home page with bio and latest release details

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// Home page for Sacean's website
+export default function Home() {
+    return <div>
+        <h1>Sacean</h1>
+        <p>Welcome to Sacean's official website!</p>
+        <h2>Latest Release</h2>
+        <p>[Latest Release Details]</p>
+        <h2>Follow Me</h2>
+        <SocialLinks />
+    </div>;
+}
