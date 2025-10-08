d4243d819d4d7e3559f2f23eb93cb55c8409101e
saceanofficial <saceanofficial@gmail.com>
2025-10-08 12:21:19 +0800

    Add SocialLinks component for Sacean EDM artist website

---
+++ components/SocialLinks.js
@@ -0,0 +1,15 @@
+export default function SocialLinks() {
+  return (
+    <div style={{ margin: '2rem 0' }}>
+      <a href="https://soundcloud.com/sacean" target="_blank" style={{ color: 'white', marginRight: '1rem' }}>SoundCloud</a>
+      <a href="https://instagram.com/SaceanOfficial" target="_blank" style={{ color: 'white', marginRight: '1rem' }}>Instagram</a>
+      <a href="https://twitter.com/SaceanOfficial" target="_blank" style={{ color: 'white', marginRight: '1rem' }}>Twitter</a>
+      <a href="https://facebook.com/sacean" target="_blank" style={{ color: 'white', marginRight: '1rem' }}>Facebook</a>
+      <a href="https://www.tiktok.com/@saceanofficial" target="_blank" style={{ color: 'white', marginRight: '1rem' }}>TikTok</a>
+      <a href="https://open.spotify.com/artist/4YgnwDKTVYILUBIOli6EqP" target="_blank" style={{ color: 'white', marginRight: '1rem' }}>Spotify</a>
+      <a href="https://itunes.apple.com/album/id/1752730279" target="_blank" style={{ color: 'white', marginRight: '1rem' }}>Apple Music</a>
+      <a href="https://www.deezer.com/album/603453192" target="_blank" style={{ color: 'white', marginRight: '1rem' }}>Deezer</a>
+      <a href="https://music.amazon.co.uk/artists/B0BZZZGJVQ/sacean" target="_blank" style={{ color: 'white', marginRight: '1rem' }}>Amazon Music</a>
+    </div>
+  );
+}
